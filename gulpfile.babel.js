015b3e0d96ee2748aac8d1f78eaf49221682c681
Teodora Kacharova <teodora.kacharova@droxic.com>
2018-10-22 15:26:26 +0300

    Add gulp-cssnano for minification

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,6 +3,7 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('gulp-autoprefixer');
+const cssnano = require('gulp-cssnano');
 
 const babel = require('gulp-babel');
 const browserSync = require("browser-sync").create();
@@ -24,7 +25,8 @@ gulp.task('default', () =>
     gulp.src('src/app.js')
         .pipe(babel({
             presets: ['@babel/env']
-        }))
+		}))
+		.pipe(cssnano())
         .pipe(gulp.dest('dist'))
 );
 
