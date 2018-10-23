38f5b149e616c6d710e37b2213788486be3ce296
Konstantin Ivanov <dev.konstantin.ivanov@gmail.com>
2018-10-23 14:34:09 +0300

    Created new paths file

---
+++ paths.js
@@ -0,0 +1,15 @@
+const path = {
+    src: './src/**/*',
+    srcHTML: './src/html/**/*.html',
+    srcSCSS: './src/styles/**/*.scss',
+    srcJS: './src/js/**/*.js',
+
+    dist: './dist/**/*',
+    distHTML: './dist/index.html',
+    distCSS: './dist/styles',
+    distJS: './dist/scripts',
+}
+
+module.exports = {
+    path: path
+}
