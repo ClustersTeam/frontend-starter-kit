322d15780ad5d40a83b6f1466141a90d2c1f4a49
Emiliya Sokolova <emiliyams@Emiliyas-MacBook-Pro.local>
2018-10-24 10:36:23 +0300

    Added stylelint task. Updated sass files.

--- tasks/tests.js
+++ tasks/tests.js
@@ -1,11 +1,27 @@
-const gulp = require('gulp');
-const size = require('gulp-size');
+const gulp = require('gulp')
+const size = require('gulp-size')
 
-const paths = require('../paths');
-const path = paths.path;
+const stylelint = require('gulp-stylelint')
 
-module.exports = function () {
-    return gulp.src(path.dist)
-        .pipe(size({title: 'File size:', showFiles: true}))
-        .pipe(gulp.dest('../dist'));
-};
+const paths = require('../paths')
+const path = paths.path
+
+gulp.task('stylelint', () => {
+  return gulp.src(path.srcSCSS).pipe(
+    stylelint({
+      reporters: [{ formatter: 'string', console: true }]
+    })
+  )
+})
+
+gulp.task('filesize', () => {
+  return gulp
+    .src(path.dist)
+    .pipe(size({ title: 'File size:', showFiles: true }))
+    .pipe(gulp.dest('../dist'))
+})
+
+module.exports = done => {
+  gulp.series('filesize', 'stylelint')
+  done()
+}
