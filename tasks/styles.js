e1ec112b62dc693127ec8a810f397987596d81ab
DESKTOP-EEUHP7H\Emiliya Sokolova <emiliya.sokolova@gmail.com>
2018-11-06 11:01:04 +0200

    added criticalcss gulp task

--- tasks/styles.js
+++ tasks/styles.js
@@ -6,6 +6,7 @@ const cssnano = require('gulp-cssnano')
 const gulpif = require('gulp-if')
 const browserSync = require('browser-sync').create()
 const autoprefixer = require('gulp-autoprefixer')
+const critical = require('critical').stream;
 
 const env = require('../environment')
 
@@ -16,11 +17,34 @@ const path = paths.path
 gulp.task('styles', () => {
   return gulp
     .src(path.srcSCSS)
-    .pipe(gulpif(env.global === 'dev', sourcemaps.init()))
+    .pipe(gulpif(env.global === 'dev', sourcemaps.init({loadMaps: true})))
     .pipe(sass({ fiber: Fiber }).on('error', sass.logError))
     .pipe(autoprefixer())
     .pipe(gulpif(env.global === 'prod', cssnano()))
-    .pipe(gulpif(env.global === 'dev', sourcemaps.write()))
+    .pipe(gulpif(env.global === 'dev', sourcemaps.write(
+      {
+        discardComments: {
+            removeAll: true
+        },
+        discardDuplicates: true,
+        discardEmpty: true,
+        minifyFontValues: true,
+        minifySelectors: true
+    })))
     .pipe(gulp.dest(path.distCSS))
     .pipe(gulpif(env.global === 'dev', browserSync.stream()))
 })
+
+// Generate & Inline Critical-path CSS
+gulp.task('criticalcss', () => {
+    return gulp.src('dist/*.html')
+        .pipe(critical({
+            base: './dist/', 
+            inline: true, 
+            css: ['dist/styles/critical.css']
+        }))
+        .on('error', function(err) { 
+            log.error(err.message); 
+        })
+        .pipe(gulp.dest('dist'));
+});
